9f98fbe8acfc07bbc4edb5097f8fe4a68647b750
jiji1007 <hajarelouali681@gmail.com>
2025-05-13 11:36:09 +0100

    Add files via upload

---
+++ Database.java
@@ -0,0 +1,21 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+	  private static final String URL = "jdbc:mysql://localhost:3306/gestion_hospitaliere"; // adapte ici ton nom de BDD
+	    private static final String USER = "root"; // Ton utilisateur MySQL
+	    private static final String PASSWORD = "hajar"; // Ton mot de passe MySQL (souvent vide en local)
+
+	    public static Connection getConnection() throws SQLException {
+	        try {
+	            Class.forName("com.mysql.cj.jdbc.Driver"); // Assure-toi d'avoir le connecteur MySQL dans ton projet
+	            return DriverManager.getConnection(URL, USER, PASSWORD);
+	        } catch (ClassNotFoundException e) {
+	            throw new SQLException("Erreur de chargement du Driver JDBC", e);
+	        }
+	    }
+	}
+
